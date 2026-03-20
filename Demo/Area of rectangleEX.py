845aa3d28559cb13d620aed07218efb7e904e1b8
unknown <08250288.sce@rub.edu.bt>
2026-03-20 15:42:31 +0600

    .\Area of rectangleEX.py

---
+++ Demo/Area of rectangleEX.py
@@ -0,0 +1,7 @@
+L=float(input("Enter the length of the rectangle:"));
+B=float(input("Enter the breadth of the rectangle"));
+area=L*B
+print("the area of the rectangle is:",area);
+print("the area of the rectangle is:",str (area));
+
+
