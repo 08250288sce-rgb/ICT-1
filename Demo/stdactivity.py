0fc542e921de737367cc25206166ae5fea8ff55c
unknown <08250288.sce@rub.edu.bt>
2026-03-20 16:14:56 +0600

    .\stdactivity.py

---
+++ Demo/stdactivity.py
@@ -0,0 +1,14 @@
+Name=input ("Enter your Name")
+print("hello", Name)
+Num1=float(input("Enter the number"))
+print(Num1)
+Num2=float(input("Enter the number"))
+print(Num2)
+addition_result=Num1+Num2
+print(addition_result)
+subtraction_result=Num1-Num2
+print(subtraction_result)
+multiplication_result=Num1*Num2
+print(multiplication_result)
+division_result=Num1/Num2
+print(division_result)
