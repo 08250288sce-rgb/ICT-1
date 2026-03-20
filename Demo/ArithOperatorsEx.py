ba022885ae82f2e5fb34d77eeeb32e7af2c5eda1
unknown <08250288.sce@rub.edu.bt>
2026-03-20 15:06:05 +0600

    .\ArithOperatorsEx.py

---
+++ Demo/ArithOperatorsEx.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+addition_result= a+b
+subtraction_result = a-b
+multiplication_results=a*b
+division_result=a/b
+exponentiation_result=a**b
+modules_results=a%b
+print(additional_result)
+print(subtraction_result)
+print(multiplication_results)
+print(division_result)
+print(exponentiation_result)
+print(modules_results)
