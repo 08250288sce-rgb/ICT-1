d7cc73c91e28fa4e29148bd37cf254b2762393ae
unknown <08250288.sce@rub.edu.bt>
2026-04-01 10:07:20 +0600

    .\listEx.py

---
+++ Demo/listEx.py
@@ -0,0 +1,26 @@
+my_list = [1 , 2 , 3 , "Hello" , 3.14 , True]
+my_repeated_list = [3]*3
+print(my_list)
+print(type(my_list))
+print(my_repeated_list)
+print(my_list[1])
+my_list.append("world")
+print(my_list)
+my_list.extend([4,5,6])
+print(my_list)
+my_list.insert(0,"start")
+print(my_list)
+my_list.remove(3)
+print(my_list)
+my_list.pop()
+print(my_list)
+del my_list[-1]
+print(my_list)
+del my_list[5]
+print(my_list)
+my_list.insert(5,False)
+print(my_list)
+my_list[0]=not_hello
+print(my_list)
+
+
