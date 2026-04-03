bbecefbae912db694160ae57ec4fbf4bcc14157b
unknown <08250288.sce@rub.edu.bt>
2026-04-03 16:14:42 +0600

    .\dictionaryEX.py

---
+++ Demo/dictionaryEX.py
@@ -0,0 +1,18 @@
+userDetails = {'id' : 1, 'userName': 'just_me'}
+print(type(userDetails))
+print(userDetails)
+location = dict(s = 'Samtse', t = 'Thimphu', p = 'Paro')
+print(location)
+print(userDetails['userName'])
+print(location.get('t'))
+userDetails['email'] = 'justme@example.com'
+print(userDetails)
+userDetails['userName'] = 'just_me_updated'
+print(userDetails)
+del location['p']
+print(location)
+deleted_value = userDetails.pop('email')
+print(deleted_value)
+del_key, del_value = userDetails.popitem()
+print(f'the deleted key is {del_key} and the deleted value is {del_value}')
+print(location)
