1e8ed7b7d40df56f00bacdfe02b80f1b71d5c7a1
unknown <08250288.sce@rub.edu.bt>
2026-04-10 16:28:59 +0600

    .\stdact_loop.py

---
+++ Demo/stdact_loop.py
@@ -0,0 +1,11 @@
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*", end=" ")
+    print()   
+for x in range(1,6):
+    for y in range(x):
+        print(y+1, end="")
+    print()
+
+
+
