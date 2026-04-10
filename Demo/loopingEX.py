c3a316453deb514cdf66dfede03a20ad62d065ed
unknown <08250288.sce@rub.edu.bt>
2026-04-10 16:23:12 +0600

    .\loopingEX.py

---
+++ Demo/loopingEX.py
@@ -0,0 +1,15 @@
+name=input("Enter your name:")
+for i in name:
+    print(i)
+li = ["Python Programming","Python Fundamentals","Python Interview Questions"]
+for x in li:
+    print(x)
+lenli=len(li)
+for x in range(lenli):
+    print(li[x])
+print()
+set_list = set(li)
+for x in set_list:
+    print(x)
+print()
+
